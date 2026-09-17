34ec8af6145fcabfd19f5c42d7533998e5705cc7
Toby Edwards Rean <tedwardsrean@outlook.com>
2026-09-17 16:00:16 +0100

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,47 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.minStack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+        let currentMin = this.minStack[this.minStack.length - 1]
+        
+        if (this.minStack.length === 0) {
+            this.minStack.push(val)
+        }
+        else if (val < currentMin) {
+            this.minStack.push(val)
+        }
+        else {
+            this.minStack.push(currentMin)
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1]
+    }
+}
